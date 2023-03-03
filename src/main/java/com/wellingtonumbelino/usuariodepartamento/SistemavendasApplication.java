3bd5d96bb3e28ca9d8dc74f7f7d7e6aa7f88a868
Wellington Umbelino <wellington.s.umbelino@gmail.com>
2023-03-02 21:49:41 -0300

    Criando mini aplicação para entender o básico do Spring Boot.

---
+++ src/main/java/com/wellingtonumbelino/usuariodepartamento/SistemavendasApplication.java
@@ -0,0 +1,13 @@
+package com.wellingtonumbelino.usuariodepartamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemavendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemavendasApplication.class, args);
+	}
+
+}
